b9f770abf664de2c635d8736d5c8a01a7b275fd4
axbravo <axbravo@pucp.pe>
2016-09-18 16:57:51 -0500

    listo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,16 +16,7 @@ class DatabaseSeeder extends Seeder
 
         // $this->call(UserTableSeeder::class);
         $this->call(RoleTableSeeder::class);
-        $this->call(ModuleTableSeeder::class);
         $this->call(TestUserTableSeeder::class);
-        $this->call(CategoryTableSeeder::class);
-        //this->call(LocalTableSeeder::class);
-        $this->call(OrganizerTableSeeder::class);
-        $this->call(politicsSeeder::class);
-        $this->call(AccessPromotionSeeder::class);
-        $this->call(ExchangeRateTableSeeder::class);
-        $this->call(ModuleAssigmentTableSeeder::class);
-        $this->call(AboutTableSeeder::class);
         $this->call(BusinessTableSeeder::class);
 
         Model::reguard();
